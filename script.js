880ef5c4fafa79e57c6ce1b567fb7c6ff6a85f14
Andrew-bot1 <andrewhayes248@gmail.com>
2025-03-17 17:03:42 -0500

    changed code

---
+++ script.js
@@ -0,0 +1,12 @@
+//display alert good aftenoon
+window.alert("Good afternoon");
+
+//declare the variabke to hold the div element
+let container = document.getElementById("container");
+//get
+//set the content of the element
+container.innerHTML = "This is the container place";
+//change the color style to red
+container.style.color = "red";
+//add the new element p into the page
+DocumentFragment.write("<p>This is new paragraph</p>");
